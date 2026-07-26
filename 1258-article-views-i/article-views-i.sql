2a8d7f992a98a27f2bf3b2c0a283958c3cc2fc90
Ashwani Kumar Bhatnagar <157265808+ashwani2004@users.noreply.github.com>
2026-07-26 16:35:33 +0530

    Time: 475 ms (23.69%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,1 +1,1 @@
-SELECT distinct author_id as id from Views where viewer_id = author_id order by author_id
+SELECT distinct author_id as id from Views where viewer_id = author_id order by author_id asc;
